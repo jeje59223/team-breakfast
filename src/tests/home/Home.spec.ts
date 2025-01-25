d1120ae20b7ef179f9e4c306fe253a85db2bec37
JEROME CNOCKAERT <10009628@mac.home>
2025-01-25 12:52:58 +0100

    feat(TEAM-BREAKFAST): add rules eslint

--- src/tests/home/Home.spec.ts
+++ src/tests/home/Home.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
-import TeamBreakfastHome from '@/components/TeamBreakfastHome';
+import TeamBreakfastHome from '@/components/TeamBreakfastHome.vue';
 
 describe('TeamBreakfastHome', () => {
   const wrapper = mount(TeamBreakfastHome);
@@ -8,7 +8,7 @@ describe('TeamBreakfastHome', () => {
   it('renders properly', () => {
     const title = wrapper.find('[tnr-id="home-title"]');
     const subtitle = wrapper.find('[tnr-id="home-subtitle"]');
-    expect(title.text()).toBe('TeamBreakfastHome');
+    expect(title.text()).toBe('Home');
     expect(subtitle.text()).toBe('Welcome to the home page');
   });
 });
